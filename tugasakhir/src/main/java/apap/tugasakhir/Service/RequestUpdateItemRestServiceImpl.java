5a9cd9b811b6595b25e0dd79637902048e15a862
farizwhyzn <farizwhyzn@gmail.com>
2021-12-10 12:28:56 +0700

    feat: 9, 10, 11 completed

--- tugasakhir/src/main/java/apap/tugasakhir/Service/RequestUpdateItemRestServiceImpl.java
+++ tugasakhir/src/main/java/apap/tugasakhir/Service/RequestUpdateItemRestServiceImpl.java
@@ -1,16 +1,20 @@
 package apap.tugasakhir.Service;
 
+import apap.tugasakhir.DTO.Item;
 import apap.tugasakhir.Model.RequestUpdateItemModel;
 import apap.tugasakhir.Repository.RequestUpdateItemDb;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Service
 public class RequestUpdateItemRestServiceImpl implements RequestUpdateItemRestService {
-//    private final WebClient webClient;
+    private WebClient webClient;
 
     @Autowired
     RequestUpdateItemDb requestUpdateItemDb;
@@ -21,4 +25,21 @@ public class RequestUpdateItemRestServiceImpl implements RequestUpdateItemRestSe
         return requestUpdateItemDb.save(requestUpdateItem);
     }
 
+    public RequestUpdateItemRestServiceImpl(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl("https://si-item.herokuapp.com").build();
+    }
+
+    @Override
+    public Mono<Item> executeRequestUpdateItem(RequestUpdateItemModel requestUpdateItem) {
+        String uuid = requestUpdateItem.getIdItem();
+        Item item = new Item();
+        item.setUuid(uuid);
+        item.setStok(requestUpdateItem.getTambahanStok().intValue());
+
+        return this.webClient.put().uri("/api/item/" + uuid)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(item)
+                .retrieve()
+                .bodyToMono(Item.class);
+    }
 }
